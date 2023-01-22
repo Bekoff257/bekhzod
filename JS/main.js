48167a0f7270d5c2d918fd2222abe4503dac05e7
Anonymous <behzodbekpolatov5@gmail.com>
2023-01-22 16:30:13 +0500

    first commit

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+// var str = "30 Days Of"
+// var str = 'JavaScript';
+// var str = '  JavaScript 30 kuni  '
+// var str = 'You cannot end a sentence with because because because is a conjunction';
+// console.log(str.length)
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.substring(1));
+// console.log(str.includes('Script'));
+// console.log(str.split());
+// console.log(str.split(','));
+// console.log(str.replace('30 Days Of Python'));
+// console.log(str.charAt(15));
+// console.log(str.charCodeAt('J'));
+// console.log(str.indexOf('a'));
+// console.log(str.lastIndexOf('a'));
+// console.log(str.indexOf('because'));
+// console.log(str.lastIndexOf('because'));
+// console.log(str.trim());
+// console.log(str. startsWith(3));
+// console.log(str.endsWith('t'));
+// console.log(str.match('a'));
+// console.log(str.concat(' JavaScript'));
+// console.log(str.repeat(2));
